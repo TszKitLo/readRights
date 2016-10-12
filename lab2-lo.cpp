11920f06f54ce61f9cf50f993ec38b57255d90b9
Tsz Kit Lo <lotszkit@pc7.cs.ucdavis.edu>
2016-10-12 15:50:25 -0700

    uid null issue

--- lab2-lo.cpp
+++ lab2-lo.cpp
@@ -10,12 +10,21 @@
 #include <pwd.h>
 #include <grp.h>
 #include <sstream>
+#include <string>
 
 unsigned int charToUnsInt(char* name){
-	std::stringstream tempStr;
-	tempStr << name;
+	//std::stringstream tempStr;
+	//tempStr << name;
 	unsigned int id;
-	tempStr >> id;
+	//tempStr >> id;
+	try{
+		id = std::stol(name);
+	}
+	catch(...){
+		std::cout << name << ": no such user" << std::endl;
+	}
+	
+	
 	return id;
 }
 
@@ -44,22 +53,34 @@ void findGroup(char* groupName, char* file){
 }
 
 void findUser(char* username, char* file){
-	passwd* pwd;
+	struct passwd *pwd;
 	pwd = getpwnam(username);
 
 	if(pwd != NULL){
 		std::cout << "username not null" << std::endl;
+		std::cout << pwd->pw_uid << std::endl;
 	} else {
-		// convert char* to unsigned int
-		unsigned int uid = charToUnsInt(username);
-
-		pwd = getpwuid(((uid_t)uid));
+		
+		uid_t id;
+		
+		try{
+			// convert char* to uid_t
+			id = (uid_t)(std::stol(username));
+			std::cout << id << std::endl;
+			pwd = getpwuid(id);
+		}
+		catch(...){
+			std::cout << username << ": no such user" << std::endl;
+			return;
+		}
+		
 		if(pwd == NULL){
 			std::cout << "uid null" << std::endl;
+			
 			return;
 		}
 			
-		std::cout << "uid not null" << std::endl;
+		std::cout << std::string(pwd->pw_name) << std::endl;
 	}
 
 	
@@ -70,12 +91,16 @@ void findUser(char* username, char* file){
 int main(int argc, char** argv)
 {	
 	// check whether user provide enoguh arguments
-	if(argc < 3){
-		std::cout << "Invalid input" << std::endl;
+	if(argc < 2){
+		std::cout << "usage: ./access [ -g ] name file1 ..." << std::endl;
 		return 1;
 	}
 
-	(strcmp( argv[1], "-g") == 0) ? findGroup(argv[2],argv[3]) : findUser(argv[1],argv[2]);
+	if((strcmp( argv[1], "-g") == 0 && argc < 4) || (strcmp( argv[1], "-g") != 0 && argc < 3)){
+		std::cout << ": need at least one file or directory!" << std::endl;
+	} else {
+		findUser(argv[1],argv[2]);
+	}
     
     return 0;
 }
