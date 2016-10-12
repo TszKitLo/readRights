62b4f68cc5d4d848031285460987b94419e50fda
Keith Lo <lotszkitkeith@gmail.com>
2016-10-11 18:47:09 -0700

    flag check

--- lab2-lo.cpp
+++ lab2-lo.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <cstring>
+
+
+void group(){
+	std::cout << "in group" << std::endl;;
+}
+
+void user(){
+	std::cout << "in user" << std::endl;;
+}
 
 int main(int argc, char** argv)
 {	
-	std::cout << argc << std::endl;
-    std::cout << argv[1] << std::endl;
+	if(argc < 3){
+		std::cout << "Invalid input" << std::endl;
+		return 1;
+	}
+
+	(strcmp( argv[1], "-g") == 0) ? group() : user();
     
     return 0;
 }
+
